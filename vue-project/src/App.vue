516c30f5001810d3f549cd55446b036ff99d1282
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-25 23:22:55 +0900

    create project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const message = "Hello Vue";
+    return { message };
+  },
+};
+</script>
+
+<style></style>
