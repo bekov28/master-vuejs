0bc13526cd6418cf427c853d6e8ada7293a482fe
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-26 23:49:15 +0900

    component created

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div>{{ message }}</div>
+  <contact></contact>
+  <button-counter></button-counter>
 </template>
 
 <script>
+import Contact from "./components/Contact.vue";
+
 export default {
   setup() {
     const message = "Hello Vue";
---
+++ vue-project/src/components/Contact.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Contact Us</h1>
+    <p>Please send an email to berd@gmail.com</p>
+  </div>
+</template>
