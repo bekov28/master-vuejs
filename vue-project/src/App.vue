f2b61ad390618a1692c92fbf494c93a5daf95d84
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-28 15:31:41 +0900

    list of contacts

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="container">
     <div>{{ message }}</div>
-    <Contact name="Berdiyor" :phone="12345678" :ownerName="contactsOwner"></Contact>
+    <div class="row">
+      <div class="col-12" v-for="contact in contacts" :key="contact.name">
+        <Contact
+          :name="contact.name"
+          :phone="contact.phone"
+          :ownerName="contact.ownerName"
+          :email="contact.email"
+          :isFavorite="contact.isFavorite"
+        ></Contact>
+      </div>
+    </div>
   </div>
-</template>s
+</template>
 
 <script setup>
+import { reactive, ref } from "vue";
 import Contact from "./components/Contact.vue";
 
 const message = "Hello Vue";
-const contactsOwner = "dotnetmastery";
+const ownerName = ref("dotnetmastery");
+
+const contacts = reactive([
+  {
+    name: "Bhrugen",
+    phone: 123342343,
+    ownerName: ownerName.value,
+    isFavorite: false,
+  },
+  {
+    name: "Bella",
+    phone: 123342332432,
+    ownerName: ownerName.value,
+    isFavorite: true,
+  },
+  {
+    name: "Ben",
+    phone: 1233422432,
+    ownerName: ownerName.value,
+    isFavorite: false,
+  },
+]);
 </script>
 
 <style></style>
--- vue-project/src/components/Contact.vue
+++ vue-project/src/components/Contact.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="bg-info rounded p-2 m-2">
-    <h3>Name: {{ name }}</h3>
-    <p>Email: {{ email }}</p>
-    <p>Phone: {{ phone }}</p>
-    <p class="float-end small" v-if="ownerName != ''">
-      *this contact info belongs to {{ ownerName }}
-    </p>
+  <div class="bg-info rounded p-2 m-1">
+    <div class="row">
+      <div class="col-12">
+        <h3>Name: {{ name }}</h3>
+        <p>Email: {{ email }}</p>
+        <p>Phone: {{ phone }}</p>
+        <p class="float-end small" v-if="ownerName != ''">
+          *this contact info belongs to {{ ownerName }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const props = defineProps({name: String, phone : Number, ownerName: String});
+const props = defineProps({
+  name: { type: String, required: true },
+  phone: Number,
+  ownerName: String,
+  email: { type: String, required: false, default: "N/A" },
+  isFavorite: Boolean,
+});
 
 const email = ref("berd@gmail.com");
 </script>
