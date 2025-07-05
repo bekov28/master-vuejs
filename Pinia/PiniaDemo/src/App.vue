fde7182bdba908633b93710f1ebea6c4d47f7351
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-05 18:20:19 +0900

    components created

--- Pinia/PiniaDemo/src/App.vue
+++ Pinia/PiniaDemo/src/App.vue
@@ -3,6 +3,21 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header><div class="wrapper">Hello World</div></header>
+  <header>
+    <div class="text-center pt-3 text-success h3">CountOpedia</div>
+    <hr />
+    <div class="row">
+      <div class="col-4 offset-1">
+        <router-link to="/" class="form-control badge rounded-pill text-bg-warning p-4">
+          Home</router-link
+        >
+      </div>
+      <div class="col-4 offset-1">
+        <router-link to="/info" class="form-control badge rounded-pill text-bg-warning p-4">
+          Info</router-link
+        >
+      </div>
+    </div>
+  </header>
   <RouterView />
 </template>
