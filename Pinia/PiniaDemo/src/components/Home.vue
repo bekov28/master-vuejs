6fd841746d41e65bb9fa063454208aff485614cc
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-05 23:25:49 +0900

    getters and actions

--- Pinia/PiniaDemo/src/components/Home.vue
+++ Pinia/PiniaDemo/src/components/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="text-center">
     <p>Let's play the game</p>
-    <h2 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h2>
+    <h2 class="text-primary pb-3">Current Score: {{ gameStore.getScore }}</h2>
     <span class="text-primary pb-3">Max Score: {{ gameStore.maxHealth }}</span>
 
     <div class="row">
       <div class="col-4 offset-1 pt-2">
-        <button class="form-control btn btn-success p-4">Increment</button>
+        <button class="form-control btn btn-success p-4" @click="Increment">Increment</button>
       </div>
       <div class="col-4 offset-1 pt-2">
-        <button class="form-control btn btn-danger p-4">Decrement</button>
+        <button class="form-control btn btn-danger p-4" @click="Decrement">Decrement</button>
       </div>
       <div class="col-5 offset-3 pt-2">
-        <button class="form-control btn btn-warning p-4">Random</button>
+        <button class="form-control btn btn-warning p-4" @click="Random">Random</button>
       </div>
     </div>
   </div>
@@ -22,4 +22,17 @@
 import { useGameStore } from "@/store/gameStore";
 
 const gameStore = useGameStore();
+
+//Note that getters are cached!!
+function Increment() {
+  gameStore.setNextAttack();
+}
+
+function Decrement() {
+  gameStore.setNextDefense();
+}
+
+function Random() {
+  Math.random() > 0.5 ? Increment() : Decrement();
+}
 </script>
--- Pinia/PiniaDemo/src/store/gameStore.js
+++ Pinia/PiniaDemo/src/store/gameStore.js
@@ -2,9 +2,28 @@ import { defineStore } from "pinia";
 
 export const useGameStore = defineStore("gameStore", {
   state: () => ({
-    score: 0,
+    score: 50,
     maxHealth: 100,
     maxAttack: 30,
-    maxDefense: 10
+    maxDefense: 10,
   }),
+  getters: {
+    //getters should be used when you want to return smth or get data, for
+    //updating - Pinia actions are better!!
+    getScore() {
+      return this.score;
+    },
+  },
+  actions: {
+    setNextAttack() {
+      let attack = Math.floor(Math.random() * this.maxAttack) + 1;
+      console.log("attack:", attack);
+      this.score += attack;
+    },
+    setNextDefense() {
+      let defense = Math.floor(Math.random() * this.maxDefense) + 1;
+      console.log("defense:", defense);
+      this.score -= defense;
+    },
+  },
 });
