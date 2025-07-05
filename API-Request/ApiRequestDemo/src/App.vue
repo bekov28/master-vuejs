191c4fde1bf63fc8ea3486b47bf90ef1c2ae33be
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-05 09:48:15 +0900

    api call

--- API-Request/ApiRequestDemo/src/App.vue
+++ API-Request/ApiRequestDemo/src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Hello World</div>
+  <h1>JSON Server Demo using Axios</h1>
+  <br /><br />
+  <div v-for="user in userObject.users" :key="user.id">
+    <h2>{{ user.id }}. {{ user.name }}</h2>
+    <p>{{ user.email }}</p>
+  </div>
 </template>
+<script setup>
+import { reactive, onMounted } from "vue";
+import axios from "axios";
+
+const userObject = reactive({
+  users: [],
+});
+
+onMounted(() => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    console.log(response.data);
+
+    userObject.users = response.data;
+  });
+});
+</script>
