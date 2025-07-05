f5ce9186b6d2e31c413114aa7f67c6c7d5196bb4
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-05 19:05:35 +0900

    defineStore

--- Pinia/PiniaDemo/src/components/Home.vue
+++ Pinia/PiniaDemo/src/components/Home.vue
@@ -1,1 +1,25 @@
-<template>Home</template>
+<template>
+  <div class="text-center">
+    <p>Let's play the game</p>
+    <h2 class="text-primary pb-3">Current Score: {{ gameStore.score }}</h2>
+    <span class="text-primary pb-3">Max Score: {{ gameStore.maxHealth }}</span>
+
+    <div class="row">
+      <div class="col-4 offset-1 pt-2">
+        <button class="form-control btn btn-success p-4">Increment</button>
+      </div>
+      <div class="col-4 offset-1 pt-2">
+        <button class="form-control btn btn-danger p-4">Decrement</button>
+      </div>
+      <div class="col-5 offset-3 pt-2">
+        <button class="form-control btn btn-warning p-4">Random</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useGameStore } from "@/store/gameStore";
+
+const gameStore = useGameStore();
+</script>
